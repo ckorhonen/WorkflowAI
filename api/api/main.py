a5d535945a4f64a7648577b1fa14edb47f168ed6
YBubu <yann.bury@gmail.com>
2025-04-22 18:13:34 +0000

    move slack router

--- api/api/main.py
+++ api/api/main.py
@@ -37,7 +37,6 @@ from .common import setup
 from .routers import (
     probes,
     run,
-    webhooks,
 )
 from .services.request_id_ctx import request_id_var
 
@@ -133,7 +132,6 @@ if WORKFLOWAI_ALLOWED_ORIGINS:
 
 app.include_router(probes.router)
 app.include_router(run.router)
-app.include_router(webhooks.router)
 
 if not _ONLY_RUN_ROUTES:
     from .main_router import main_router
