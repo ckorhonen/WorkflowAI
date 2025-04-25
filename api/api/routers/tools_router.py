bd3a8684bb0d6be4d7e1d2d55fb3508993c44c5d
YBubu <yann.bury@gmail.com>
2025-04-25 15:14:32 +0000

    remove task dep in router

--- api/api/routers/tools_router.py
+++ api/api/routers/tools_router.py
@@ -1,23 +1,20 @@
 from fastapi import APIRouter, HTTPException
 
 from api.dependencies.services import ToolsServiceDep
-from api.dependencies.task_info import TaskInfoDep
 from api.schemas.tools_schema import ToolParamsRequest, ToolResponse
 from core.storage import ObjectNotFoundException
 
 router = APIRouter(prefix="/agents/{task_id}/tools")
 
-# The endpoint is this router have a 'task' dependency that just checks that the task in url exists but the task is not used since we fetch all the tools from the tenant.
-
 
 @router.get("")
-async def list_tools(task: TaskInfoDep, tools_service: ToolsServiceDep) -> list[ToolResponse]:
+async def list_tools(tools_service: ToolsServiceDep) -> list[ToolResponse]:
     tools = await tools_service.list_tools()
     return [ToolResponse.from_domain(tool) for tool in tools]
 
 
 @router.get("/{id}")
-async def get_tool(id: str, task: TaskInfoDep, tools_service: ToolsServiceDep) -> ToolResponse:
+async def get_tool(id: str, tools_service: ToolsServiceDep) -> ToolResponse:
     try:
         tool = await tools_service.get_tool_by_id(id)
         return ToolResponse.from_domain(tool)
@@ -26,7 +23,7 @@ async def get_tool(id: str, task: TaskInfoDep, tools_service: ToolsServiceDep) -
 
 
 @router.post("")
-async def create_tool(tool: ToolParamsRequest, task: TaskInfoDep, tools_service: ToolsServiceDep) -> ToolResponse:
+async def create_tool(tool: ToolParamsRequest, tools_service: ToolsServiceDep) -> ToolResponse:
     try:
         created_tool = await tools_service.create_tool(tool.name, tool.description, tool.input_schema)
         return ToolResponse.from_domain(created_tool)
@@ -38,7 +35,6 @@ async def create_tool(tool: ToolParamsRequest, task: TaskInfoDep, tools_service:
 async def update_tool(
     id: str,
     tool: ToolParamsRequest,
-    task: TaskInfoDep,
     tools_service: ToolsServiceDep,
 ) -> ToolResponse:
     try:
@@ -49,7 +45,7 @@ async def update_tool(
 
 
 @router.delete("/{id}", status_code=204)
-async def delete_tool(id: str, task: TaskInfoDep, tools_service: ToolsServiceDep):
+async def delete_tool(id: str, tools_service: ToolsServiceDep):
     try:
         await tools_service.delete_tool(id)
     except ObjectNotFoundException:
