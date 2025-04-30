b7bfafabd49721558f48f6e2c9c0076f1e169134
Guillaume Aquilina <guillaume@workflowai.com>
2025-04-30 14:31:28 -0400

    test: add openai proxy test structured output

--- api/tests/integration/openai_proxy/openai_proxy_test.py
+++ api/tests/integration/openai_proxy/openai_proxy_test.py
@@ -67,3 +67,33 @@ async def test_raw_json_mode_array(test_client: IntegrationTestClient, openai_cl
     run = await test_client.fetch_run({"id": task_id}, run_id=run_id, v1=True)
     assert run["id"] == run_id
     assert run["task_output"] == [{"whatever": "Hello world"}]
+
+
+async def test_with_json_schema(test_client: IntegrationTestClient, openai_client: AsyncOpenAI):
+    test_client.mock_openai_call(raw_content='{"whatever": "Hello world"}')
+
+    res = await openai_client.chat.completions.create(
+        model="gpt-4o",
+        messages=[{"role": "user", "content": "Hello, world!"}],
+        response_format={
+            "type": "json_schema",
+            "json_schema": {
+                "name": "test",
+                "schema": {
+                    "type": "object",
+                    "properties": {"whatever": {"type": "string"}},
+                },
+            },
+        },
+    )
+    assert res.choices[0].message.content == '{"whatever": "Hello world"}'
+
+    await test_client.wait_for_completed_tasks()
+
+    task_id, run_id = res.id.split("/")
+    run = await test_client.fetch_run({"id": task_id}, run_id=run_id, v1=True)
+    assert run["id"] == run_id
+    assert run["task_output"] == {"whatever": "Hello world"}
+
+    agent = await test_client.get(f"/_/agents/{task_id}/schemas/1")
+    assert agent["output_schema"]["json_schema"] == {"type": "object", "properties": {"whatever": {"type": "string"}}}
