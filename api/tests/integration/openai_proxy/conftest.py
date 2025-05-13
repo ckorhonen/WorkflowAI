ef8673a18405fdc9f086911f15bfc8b28d0d8c2e
Guillaume Aquilina <guillaume@workflowai.com>
2025-05-13 15:17:54 -0400

    test: add test for anthropic system message

---
+++ api/tests/integration/openai_proxy/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from openai import AsyncOpenAI
+
+from tests.integration.common import IntegrationTestClient
+from tests.integration.conftest import _TEST_JWT  # pyright: ignore [reportPrivateUsage]
+
+
+@pytest.fixture()
+def openai_client(test_client: IntegrationTestClient):
+    yield AsyncOpenAI(http_client=test_client.int_api_client, api_key=_TEST_JWT)
