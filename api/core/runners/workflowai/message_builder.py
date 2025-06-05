0b567334f911f431cf373aa0ea030e34dbf45d58
Guillaume Aquilina <guillaume@workflowai.com>
2025-06-05 16:32:53 -0400

    fix: issue with message validation

--- api/core/runners/workflowai/message_builder.py
+++ api/core/runners/workflowai/message_builder.py
@@ -4,7 +4,7 @@ from typing import Any
 
 from pydantic import ValidationError
 
-from api.services.runs._stored_message import StoredMessages
+from api.services.runs._stored_message import StoredMessage, StoredMessages
 from core.domain.errors import BadRequestError
 from core.domain.fields.file import File, FileKind
 from core.domain.message import Message, MessageContent
@@ -104,13 +104,14 @@ class MessageBuilder:
     async def extract(self, input: Any):
         # No matter what, the input should be a valid Messages object
         if isinstance(input, list):
-            input = {"workflowai.messages": input}
-        try:
-            # Stored messages allows extras
-            messages = StoredMessages.model_validate(input)
-        except ValidationError as e:
-            # Capturing for now just in case
-            raise BadRequestError(f"Input is not a valid list of messages: {str(e)}", capture=True) from e
+            messages = StoredMessages(messages=[StoredMessage.model_validate(m) for m in input])  # type:ignore
+        else:
+            try:
+                # Stored messages allows extras
+                messages = StoredMessages.model_validate(input)
+            except ValidationError as e:
+                # Capturing for now just in case
+                raise BadRequestError(f"Input is not a valid list of messages: {str(e)}", capture=True) from e
 
         if self._input_schema.version == RawMessagesSchema.version:
             # Version messages are not templated since there is no field in the input schema
